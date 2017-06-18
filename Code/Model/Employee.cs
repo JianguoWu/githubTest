945918fcc5965abfd4fd31e6a5887222c40c9b9c
JianguoWu <1662835025@qq.com>
2017-06-18 16:46:24 +0800

    1.0新建Model层

---
+++ Code/Model/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Employee
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+    }
+}
